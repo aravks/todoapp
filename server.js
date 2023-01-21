59a7ef0270c392e06666f5708b101ccafe82ac67
AravindKS <aravindvasan1@gmail.com>
2023-01-21 13:17:27 +0530

    Changes made for production

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ routes(app);
 
 database();
 
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 app.listen(PORT, () => {
   console.log(`The server is running on ${PORT}`);
 });
